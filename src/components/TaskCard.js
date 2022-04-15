24d366985dc188a914ede7b50be9cf5d72f3c19b
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-04-15 15:28:45 -0400

    updated card

--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -5,9 +5,12 @@ const TaskCard = () => {
 return (
 <div className="first hero">
   <img className="location-img" src="http://tile.loc.gov/image-services/iiif/service:pnp:highsm:38100:38110/full/pct:25/0/default.jpg" alt="location"/>
-  <div className="hero-description-bk"></div>
+  <div className="location-description"></div>
   <div className="hero-logo">
-    <img src="https://miscmedia-9gag-fun.9cache.com/images/thumbnail-facebook/1557291375.3948_Dy2yZu_n.jpg" alt=""/>
+    <img src='https://i.imgur.com/CoSoBtE.png' alt=""/>
+  </div>
+  <div className="location-title">
+    <p>Description</p>
   </div>
   <div className="hero-description">
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
@@ -15,9 +18,9 @@ return (
   <div  className="task-date">
     <p>20.02.2019</p>
   </div>
-  <div className="hero-btn">
+  {/* <div className="hero-btn">
    <Link to='/signin'></Link>
-  </div>
+  </div> */}
 </div>
 )
 }
