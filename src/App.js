f4991148af4f31598b64d130441f7d108dabf033
Huan <huan.zeng1@gmail.com>
2022-04-18 16:58:55 -0400

    fix render on profile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import './style/App.css';
-import {useState, useEffect} from 'react'
+import './style/App.css'
+import { useState, useEffect } from 'react'
 import { Route, Routes } from 'react-router'
 import { CheckSession } from './services/Auth'
 
@@ -9,7 +9,6 @@ import SignIn from './pages/SignIn'
 import Home from './pages/Home'
 
 const App = () => {
-
   const [authenticated, toggleAuthenticated] = useState(false)
   const [user, setUser] = useState(null)
 
@@ -21,16 +20,20 @@ const App = () => {
   }
 
   const checkToken = async () => {
+    console.log('check token is called')
     //If a token exists, sends token to localStorage to persist logged in user
     const user = await CheckSession()
+    console.log(user)
     setUser(user)
     toggleAuthenticated(true)
+    console.log(authenticated)
   }
 
   useEffect(() => {
     const token = localStorage.getItem('token')
     // Check if token exists before requesting to validate the token
     if (token) {
+      console.log('token valided')
       checkToken()
     }
   }, [])
@@ -44,13 +47,30 @@ const App = () => {
       />
       <main>
         <Routes>
-          <Route path="/signin" element={<SignIn setUser={setUser} toggleAuthenticated={toggleAuthenticated}/>} />
-          <Route path="/" element={<Home user={user} authenticated={authenticated}/>} />
-          <Route path="/profile/:id" element={<Profile user={user} authenticated={authenticated}/>} />
+          <Route
+            path="/signin"
+            element={
+              <SignIn
+                setUser={setUser}
+                toggleAuthenticated={toggleAuthenticated}
+              />
+            }
+          />
+
+          <Route
+            path="/"
+            element={<Home user={user} authenticated={authenticated} />}
+          />
+          {user && (
+            <Route
+              path="/profile"
+              element={<Profile user={user} authenticated={authenticated} />}
+            />
+          )}
         </Routes>
       </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
