bebe4e8c781786eae466ea8aa330ad9f216ccc86
Eric Lantz <77892536+ericmlantz@users.noreply.github.com>
2022-04-18 14:47:19 -0400

    Added sign in and out capabilities and altered nav on signin. Auth staying when refreshing error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './style/App.css';
-
+import {useState, useEffect} from 'react'
 import { Route, Routes } from 'react-router'
+import { CheckSession } from './services/Auth'
 
 import Nav from './components/Nav'
 import Profile from './pages/Profile'
@@ -9,15 +10,43 @@ import Home from './pages/Home'
 
 const App = () => {
 
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    //Reset all auth related state and clear localStorage
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    //If a token exists, sends token to localStorage to persist logged in user
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    // Check if token exists before requesting to validate the token
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
     <div className="App">
-      <Nav />
+      <Nav
+        authenticated={authenticated}
+        user={user}
+        handleLogOut={handleLogOut}
+      />
       <main>
         <Routes>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/profile/:id" element={<Profile />} />
-          
+          <Route path="/signin" element={<SignIn setUser={setUser} toggleAuthenticated={toggleAuthenticated}/>} />
+          <Route path="/" element={<Home user={user} authenticated={authenticated}/>} />
+          <Route path="/profile/:id" element={<Profile user={user} authenticated={authenticated}/>} />
         </Routes>
       </main>
     </div>
