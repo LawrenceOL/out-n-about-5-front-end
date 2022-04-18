79f6ec8d6c054fd7b92319c506dca214b2c85bf3
Huan <huan.zeng1@gmail.com>
2022-04-18 15:06:55 -0400

    pull from main

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,42 +1,42 @@
-import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import { letterSpacing } from "@mui/system";
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import { letterSpacing } from '@mui/system'
 
 const Home = () => {
   //Game start search
-  let coordinates = [];
-  const [locations, setLocations] = useState([]);
-  const [locationsFound, setLocationsFound] = useState(false);
+  let coordinates = []
+  const [locations, setLocations] = useState([])
+  const [locationsFound, setLocationsFound] = useState(false)
 
   const getLocations = async (e) => {
     const response = await axios.post(
       `https://overpass-api.de/api/interpreter?data=`,
-      `[out:json]; node(around:8000.00,${coordinates[0]}, ${coordinates[1]})["amenity"="restaurant"];
+      `[out:json];node(around:8000.00,${coordinates[0]}, ${coordinates[1]})["amenity"="restaurant"];
         out body;
         `
-    );
-    console.log(response.data.elements);
-    setLocations(response.data.elements);
-    setLocationsFound(true);
-  };
+    )
+    console.log(response.data.elements)
+    setLocations(response.data.elements)
+    setLocationsFound(true)
+  }
 
   function getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(showPosition)
     } else {
-      alert("Geolocation is not supported by this browser.");
+      alert('Geolocation is not supported by this browser.')
     }
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition)
   }
 
   function showPosition(position) {
-    coordinates = [position.coords.latitude, position.coords.longitude];
-    console.log(coordinates);
-    getLocations();
+    coordinates = [position.coords.latitude, position.coords.longitude]
+    console.log(coordinates)
+    getLocations()
   }
 
-  let navigate = useNavigate();
+  let navigate = useNavigate()
 
   return (
     <div className="home-page">
@@ -47,15 +47,11 @@ const Home = () => {
 
       {locationsFound && (
         <div>
-          <p>"We found activities: "</p>
-
-          {locations.map((locations) => (
-            <p>{locations.tags.name}</p>
-          ))}
+          <p>${locations}</p>
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
