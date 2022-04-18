c8e9ef11b8d75e6cd1e78be0f77b1a25db1f6d8d
Huan <huan.zeng1@gmail.com>
2022-04-18 13:02:34 -0400

    added profile data and update btn

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,12 @@
-import { GetProfile } from '../services/UserServices'
+import { GetProfile, UpdateProfile } from '../services/UserServices'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 const Profile = () => {
   let { id } = useParams()
   const [profile, setProfile] = useState({})
+  const [formStatus, setFormStatus] = useState(true)
+  const [updateBtn, setUpdateBtn] = useState('Enable Update')
 
   useEffect(() => {
     getProfile(id)
@@ -14,23 +16,104 @@ const Profile = () => {
     const res = await GetProfile(id)
     setProfile(res)
   }
-  console.log(profile)
+
+  const updateProfile = async (id, data) => {
+    const res = await UpdateProfile(id, data)
+    getProfile(id)
+  }
+
+  const enableUpdate = (e) => {
+    e.preventDefault()
+
+    if (updateBtn === 'Update') {
+      console.log(profile)
+      updateProfile(id, profile)
+
+      setFormStatus(true)
+      setUpdateBtn('Enable Update')
+    }
+
+    if (formStatus) {
+      setFormStatus(false)
+      setUpdateBtn('Update')
+    } else {
+      setFormStatus(true)
+    }
+  }
+
+  const handleChange = (e) => {
+    setProfile({ ...profile, [e.target.name]: e.target.value })
+  }
 
   return (
     <div className="profile-page">
       <h1>Profile Page</h1>
       <form>
         <label htmlFor="userName">User Name:</label>
-        <input type="text" id="userName" value={profile.username} />
-        <label htmlFor="password"></label>
+        <input
+          onChange={handleChange}
+          type="text"
+          id="userName"
+          name="username"
+          value={profile.username}
+          readOnly={formStatus}
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          onChange={handleChange}
+          type="password"
+          id="password"
+          name="password"
+          value={profile.password}
+          readOnly={formStatus}
+        />
         <label htmlFor="firstName">First Name:</label>
-        <input id="firstName" type="text" value={profile.firstName} readOnly />
+        <input
+          onChange={handleChange}
+          id="firstName"
+          type="text"
+          name="firstName"
+          value={profile.firstName}
+          readOnly={formStatus}
+        />
         <label htmlFor="lastName">Last Name:</label>
-        <input type="text" id="lastName" value={profile.lastName} readOnly />
+        <input
+          onChange={handleChange}
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={profile.lastName}
+          readOnly={formStatus}
+        />
         <label htmlFor="email">E-mail:</label>
-        <input type="text" id="email" value={profile.email} readOnly />
+        <input
+          onChange={handleChange}
+          type="text"
+          id="email"
+          name="email"
+          value={profile.email}
+          readOnly={formStatus}
+        />
         <label htmlFor="location">Location:</label>
-        <input type="text" id="location" value={profile.location} readOnly />
+        <input
+          onChange={handleChange}
+          type="text"
+          id="location"
+          name="location"
+          value={profile.location}
+          readOnly={formStatus}
+        />
+        <label htmlFor="score">Score:</label>
+        <input
+          onChange={handleChange}
+          type="text"
+          id="score"
+          name="score"
+          value={profile.score}
+          readOnly={formStatus}
+        />
+
+        <button onClick={enableUpdate}>{updateBtn}</button>
       </form>
     </div>
   )
